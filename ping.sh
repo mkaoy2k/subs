035a517f5b53a08fbcc23be84a1138d5a4315385
mkaoy2k <mkaoy2k@gmail.com>
2025-06-05 20:15:06 -0700

    2025-06-05 Release

--- ping.sh
+++ ping.sh
@@ -1,12 +1,22 @@
-OPS_SVR="https://sharp-planets-clean.loca.lt"
+# Ping Operations server
+# OPS_SVR="https://bold-squirrel-vertically.ngrok-free.app"
+OPS_SVR="http://127.0.0.1:8501"
 while true
     do 
         echo "================================================"
         echo "ping $OPS_SVR/about ..."
         curl "$OPS_SVR/about"
+        echo $(date +%m/%d/%y-%H:%M:%S)
+        # Sleep for a random duration between 1 to 3 minutes (60-180 seconds)
+        echo "Sleep for a random duration between 1 to 3 minutes (60-180 seconds) ..."
+        sleep $((60 + RANDOM % 121))
+ 
         echo "================================================"
-        sleep 312
         echo "ping $OPS_SVR/faq..."
         curl "$OPS_SVR/faq"
-        sleep 321
-    done
+        echo $(date +%m/%d/%y-%H:%M:%S)
+        # Sleep for a random duration between 1 to 3 minutes (60-180 seconds)
+        echo "Sleep for a random duration between 1 to 3 minutes (60-180 seconds) ..."
+        sleep $((60 + RANDOM % 121))
+
+    done    
